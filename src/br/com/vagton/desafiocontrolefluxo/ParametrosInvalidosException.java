cf74b631ace621f3cd7dc27edc0f168d27e3e958
Vagton Alves Ferreira <vagton.ferreira@gmail.com>
2024-08-30 01:31:07 -0300

    Ajustes de Mensagens

--- src/br/com/vagton/desafiocontrolefluxo/ParametrosInvalidosException.java
+++ src/br/com/vagton/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -2,13 +2,8 @@ package br.com.vagton.desafiocontrolefluxo;
 
 public class ParametrosInvalidosException extends Exception {
 
-	    // Construtor padrão
-	    //public ParametrosInvalidosException() {
-	    //   super("O Parámetro informado é inválido. Ele deve ser um número inteiro.");
-	    //}
-
-	    // Construtor que permite uma mensagem personalizada
-	    public ParametrosInvalidosException(String mensagem) {
-	        super(mensagem);
-	    }
+    // Construtor que permite uma mensagem personalizada
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
 }
